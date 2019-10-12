fa620894838c07deff7d85276dab0c5f4ed2b259
Mitul Panchal <imitulpanchal@gmail.com>
2019-10-12 20:37:25 +0530

    Initial checkin'

---
+++ newsapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from newsapi import NewsApiClient
+
+# Create your views here. 
+# API Key : 52af66723d7a4b10b0086de9d5f1ff54
+
+def index(request):
+
+    newsapi = NewsApiClient(api_key = '52af66723d7a4b10b0086de9d5f1ff54')
+    top = newsapi.get_top_headlines(sources = 'techcrunch')
+
+    l = top['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(l)):
+        f = l[i]
+        news.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+
+    myList = zip(news, desc, img)
+
+    return render(request, 'index.html', context = {"mylist": myList})
